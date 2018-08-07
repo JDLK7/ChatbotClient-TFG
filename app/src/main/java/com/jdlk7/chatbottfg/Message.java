7ea1a3eb742e9b64c98fa54a612f014b4c2cf08e
Jose Domenech Leal <josesguay@gmail.com>
2018-08-07 13:12:28 +0200

    Modificada clase Message para aceptar acciones

--- app/src/main/java/com/jdlk7/chatbottfg/Message.java
+++ app/src/main/java/com/jdlk7/chatbottfg/Message.java
@@ -1,13 +1,24 @@
 package com.jdlk7.chatbottfg;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Message {
 
     private String message;
     private boolean isUser;
+    private List<Action> mActions;
 
     public Message(String message, boolean isUser) {
         this.message = message;
         this.isUser = isUser;
+        mActions = new ArrayList<Action>();
+    }
+
+    public Message(String message, boolean isUser, List<Action> actions) {
+        this.message = message;
+        this.isUser = isUser;
+        mActions = actions;
     }
 
     public String getMessage() {
@@ -24,5 +35,13 @@ public class Message {
 
     public void setIsUserMsg(boolean isUser) {
         this.isUser = isUser;
+    }
+
+    public List<Action> getActions() {
+        return mActions;
+    }
+
+    public boolean hasActions() {
+        return mActions.size() != 0;
     }
 }
